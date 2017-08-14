42c69c2db8fbc12d0002180c257b14ea0d5e385c
HelixCodes <ethan.ontong@gmail.com>
2017-08-14 20:28:14 +0200

    Added CUstomMode AA's
    Visuals for CUstomMode AA's setting in HvH tab

--- src/ATGUI/Tabs/hvhtab.cpp
+++ src/ATGUI/Tabs/hvhtab.cpp
@@ -4,17 +4,29 @@ void HvH::RenderTab()
 {
 	const char* yTypes[] = {
 			"SLOW SPIN", "FAST SPIN", "JITTER", "BACKJITTER", "SIDE", "BACKWARDS", "FORWARDS", "LEFT", "RIGHT", "STATIC", "STATIC JITTER", "STATIC SMALL JITTER", "LUA", "LUA2", "CASUALAA",// safe
-			"LISP", "LISP SIDE", "LISP JITTER", "ANGEL BACKWARDS", "ANGEL INVERSE", "ANGEL SPIN", "LOWERBODY", "LBYONGROUND", "LUA UNCLAMPED", "LUA UNCLAMPED2", // untrusted
+			"LISP", "LISP SIDE", "LISP JITTER", "ANGEL BACKWARDS", "ANGEL INVERSE", "ANGEL SPIN", "LOWERBODY", "LBYONGROUND", "LUA UNCLAMPED", "LUA UNCLAMPED2", "CUSTOM1", "CUSTOM2" // untrusted
 	};
 
 	const char* xTypes[] = {
-			"UP", "DOWN", "DANCE", "FRONT", "LUA", // safe
+			"UP", "DOWN", "DANCE", "FRONT", "LUA", "CUSTOM", // safe
 			"FAKE UP", "FAKE DOWN", "LISP DOWN", "ANGEL DOWN", "ANGEL UP", "LUA UNCLAMPED" // untrusted
 	};
 
 	const char* resolverModes[] = {
 			"OFF", "FORCE", "DELTA", "STEADY", "TICKMODULO", "POSEPARAM", "ALL"
 	};
+	
+	 const char* customAAModes[] = {
+            "NORMAL", "SPIN", "JITTER"
+    	};
+
+    	const char* customAAModes2[] = {
+            "NORMAL", "SPIN", "JITTER"
+    	};
+
+    	const char* customPitches[] = {
+            "NORMAL", "JITTER"
+    	};
 	
 	ImGui::Columns(2, NULL, true);
 	{
@@ -83,14 +95,21 @@ void HvH::RenderTab()
 				ImGui::Separator();
 				ImGui::Checkbox(XORSTR("Knife"), &Settings::AntiAim::AutoDisable::knifeHeld);
 				ImGui::Checkbox(XORSTR("No Enemy"), &Settings::AntiAim::AutoDisable::noEnemy);
-
+				ImGui::Separator();
+				ImGui::Text("Other");
+				ImGui::Separator();
 				ImGui::Columns(1);
+				ImGui::Checkbox("Dynamic", &Settings::AntiAim::Yaw::dynamic);
+				SetTooltip("Antiaim at target");
+				ImGui::Checkbox("Moonwalk", &Settings::AntiAim::moonwalk);
+				SetTooltip("Makes you slide visually");
 				ImGui::Separator();
 				ImGui::Text(XORSTR("Edging"));
 				ImGui::Separator();
 				ImGui::Columns(2, NULL, true);
 				{
 					ImGui::Checkbox(XORSTR("Enabled"), &Settings::AntiAim::HeadEdge::enabled);
+					SetTooltip("Aims your head into the closest wall");
 				}
 				ImGui::NextColumn();
 				{
@@ -187,6 +206,85 @@ void HvH::RenderTab()
 					ImGui::InputTextMultiline(XORSTR("##LUAY2"), Settings::AntiAim::Lua::scriptY2, sizeof(Settings::AntiAim::Lua::scriptY2));
 				}
 			}
+			 ImGui::Separator();
+            if (Settings::AntiAim::Yaw::type == AntiAimType_Y::CUSTOM1 || Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::CUSTOM1)
+            {
+                ImGui::Text("Custom1 Yaw");
+                ImGui::Combo("##CUSTOMAAMODE", (int*)& Settings::AntiAim::Custom::YawOne::mode, customAAModes, IM_ARRAYSIZE(customAAModes));
+                if (Settings::AntiAim::Custom::YawOne::mode == AntiAim_CustomModes::NORMAL)
+                {
+                    ImGui::SliderFloat("##CUSTOMAAANGLE", &Settings::AntiAim::Custom::YawOne::angle, -180, 180, "Angle: %0.f");
+                    ImGui::Checkbox("Static", &Settings::AntiAim::Custom::YawOne::isStatic);
+                    SetTooltip("Ignore player's eye angle");
+                }
+                else if (Settings::AntiAim::Custom::YawOne::mode == AntiAim_CustomModes::JITTER)
+                {
+                    ImGui::SliderFloat("##JITTERRANGEMIN", &Settings::AntiAim::Custom::YawOne::jitterMin, -180, 180, "Min: %0.f");
+                    ImGui::SliderFloat("##JITTERRANGEMAX", &Settings::AntiAim::Custom::YawOne::jitterMax, -180, 180, "Max: %0.f");
+                    ImGui::Checkbox("Static", &Settings::AntiAim::Custom::YawOne::isStatic);
+                    SetTooltip("Ignore player's eye angle");
+                }
+                else if (Settings::AntiAim::Custom::YawOne::mode == AntiAim_CustomModes::SPIN)
+                {
+                    ImGui::Checkbox("Slow Spin", &Settings::AntiAim::Custom::YawOne::slowSpin);
+                    SetTooltip("Reduce spin speed");
+                    if (Settings::AntiAim::Custom::YawOne::slowSpin) {
+                        ImGui::SliderFloat("##SPINSLOW", &Settings::AntiAim::Custom::YawOne::spinFactor, 1, 25, "Slow: %0.f");
+                    }
+                    else
+                    {
+                        ImGui::SliderFloat("##SPINSPEED", &Settings::AntiAim::Custom::YawOne::spinFactor, 1, 25, "Speed: %0.f");
+                    }
+                }
+                ImGui::Checkbox("Avoid LBY", &Settings::AntiAim::Custom::YawOne::avoidLBY);
+            }
+            if (Settings::AntiAim::Yaw::type == AntiAimType_Y::CUSTOM2 || Settings::AntiAim::Yaw::typeFake == AntiAimType_Y::CUSTOM2)
+            {
+                ImGui::Text("Custom2 Yaw");
+                ImGui::Combo("##CUSTOMAAMODE2", (int*)& Settings::AntiAim::Custom::YawTwo::mode, customAAModes2, IM_ARRAYSIZE(customAAModes2));
+                if (Settings::AntiAim::Custom::YawTwo::mode == AntiAim_CustomModes::NORMAL)
+                {
+                    ImGui::SliderFloat("##CUSTOMAAANGLE2", &Settings::AntiAim::Custom::YawTwo::angle, -180, 180, "Angle: %0.f");
+                    ImGui::Checkbox("Static", &Settings::AntiAim::Custom::YawTwo::isStatic);
+                    SetTooltip("Ignore player's eye angle");
+                }
+                else if (Settings::AntiAim::Custom::YawTwo::mode == AntiAim_CustomModes::JITTER)
+                {
+                    ImGui::SliderFloat("##JITTERRANGEMIN2", &Settings::AntiAim::Custom::YawTwo::jitterMin, -180, 180, "Min: %0.f");
+                    ImGui::SliderFloat("##JITTERRANGEMAX2", &Settings::AntiAim::Custom::YawTwo::jitterMax, -180, 180, "Max: %0.f");
+                    ImGui::Checkbox("Static", &Settings::AntiAim::Custom::YawTwo::isStatic);
+                    SetTooltip("Ignore player's eye angle");
+                }
+                else if (Settings::AntiAim::Custom::YawTwo::mode == AntiAim_CustomModes::SPIN)
+                {
+                    ImGui::Checkbox("Slow Spin", &Settings::AntiAim::Custom::YawTwo::slowSpin);
+                    SetTooltip("Reduce spin speed");
+                    if (Settings::AntiAim::Custom::YawOne::slowSpin) {
+                        ImGui::SliderFloat("##SPINSLOW2", &Settings::AntiAim::Custom::YawTwo::spinFactor, 1, 25, "Slow: %0.f");
+                    }
+                    else
+                    {
+                        ImGui::SliderFloat("##SPINSPEED2", &Settings::AntiAim::Custom::YawTwo::spinFactor, 1, 25, "Speed: %0.f");
+                    }
+                }
+                ImGui::Checkbox("Avoid LBY", &Settings::AntiAim::Custom::YawTwo::avoidLBY);
+            }
+						
+	if (Settings::AntiAim::Pitch::type == AntiAimType_X::CUSTOM)
+            {
+                ImGui::Text("Custom Pitch");
+                ImGui::Combo("##CUSTOMPITCHES", (int*)& Settings::AntiAim::Custom::Pitch::mode, customPitches, IM_ARRAYSIZE(customPitches));
+                if (Settings::AntiAim::Custom::Pitch::mode == AntiAim_CustomPitches::NORMAL)
+                {
+                    ImGui::SliderFloat("##CUSTOMPITCHANGLE", &Settings::AntiAim::Custom::Pitch::angle, -89, 89, "Angle: %0.f");
+                }
+                else if (Settings::AntiAim::Custom::Pitch::mode == AntiAim_CustomPitches::JITTER)
+                {
+                    ImGui::SliderFloat("##CUSTOMPITCHJITTERMIN", &Settings::AntiAim::Custom::Pitch::jitterMin, -89, 89, "Min: %0.f");
+                    ImGui::SliderFloat("##CUSTOMPITCHJITTERMAX", &Settings::AntiAim::Custom::Pitch::jitterMax, -89, 89, "Max: %0.f");
+                }
+            }
+
 			ImGui::EndChild();
 		}
 	}
