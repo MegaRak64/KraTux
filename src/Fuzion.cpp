1562e39138b1b9008ad8058fdb453f830ff3c1a0
MegaRak64 <megarostik12@gmail.com>
2017-08-17 00:16:41 +0900

    Upgrade

--- src/Fuzion.cpp
+++ src/Fuzion.cpp
@@ -5,7 +5,7 @@
 
 static EventListener* eventListener = nullptr;
 // The Below Line is Set by the Build script. Keep this on Line 8.
-char buildID[] = "eIaD9bwI0QMpVLU2mwcSj51mDJG1rz9K"; // Line Set by build script
+char buildID[] = "tgAnr8zptP4QhLAhnbfMyDGdNaZlsgGp"; // Line Set by build script
 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 bool preload = false;
 bool isShuttingDown = false;
