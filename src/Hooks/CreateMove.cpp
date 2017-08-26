af2b61aef286cf47ae0d0b9474c3c1027b493cb0
HelixCodes <ethan.ontong@gmail.com>
2017-08-26 10:46:50 +0200

    Update CreateMove.cpp

--- src/Hooks/CreateMove.cpp
+++ src/Hooks/CreateMove.cpp
@@ -34,7 +34,8 @@ bool Hooks::CreateMove(void* thisptr, float flInputSampleTime, CUserCmd* cmd)
 		ESP::CreateMove(cmd);
 		Walkbot::CreateMove(cmd);
 		PredictionSystem::EndPrediction();
-
+		TracerEffect::CreateMove(cmd);
+		
 		EdgeJump::PostPredictionCreateMove(cmd);
 
 		if (CreateMove::sendPacket)
